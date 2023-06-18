678846dfda13420285a1f8719b10d703652e9360
Amanda Ramos <amandaramosdasilva@yahoo.com.br>
2023-06-17 21:37:14 -0300

    aprendendo forms e reactive forms

--- angular-injecao-de-dependencia/src/app/app.module.ts
+++ angular-injecao-de-dependencia/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FoodListComponent } from './shared/food-list/food-list.component';
 import { FoodAddComponent } from './shared/food-add/food-add.component';
 import { FormsModule } from '@angular/forms';
+import { FormsComponent } from './forms/forms.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FoodListComponent,
-    FoodAddComponent
+    FoodAddComponent,
+    FormsComponent,
+    ReactiveFormsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
